b7500d80ef420fa718d6e1f36a2ddbdee7a10c59
Patrick Godbey <mrpatg@gmail.com>
2018-06-28 09:05:05 -0400

    shortcuts

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ const mainMenuTemplate = [
             },
             {
                 label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
                 click(){
                     app.quit();
                 }
